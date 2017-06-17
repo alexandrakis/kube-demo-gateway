2e909b363edc51d6024a7f92763e8839632ceb72
Alexandros Alexandrakis <a.alexandrakis@datamation.gr>
2017-06-17 03:16:32 +0300

    groovy like jenkins file

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,46 +1,26 @@
 pipeline {
     agent any
 
-    stages {
-        stage('Test') {
-            steps {
-                echo 'Testing...'
-                sh '/usr/local/bin/mvn clean test'
-            }
-        }
-        stage('Build Jar') {
-            steps {
-                echo 'Building Jar file...'
-                sh '/usr/local/bin/mvn clean package'
-                archiveArtifacts artifacts: '**/target/*.jar', fingerprint: true
-                junit 'target/surefire-reports/*.xml'
-            }
-        }
-        stage('Build Docker Image') {
-            steps {
-                echo 'Building docker image....'
-                sh '/usr/local/bin/docker build -t kube-demo-plugin .'
-                echo 'Taging docker image....'
-                sh '/usr/local/bin/docker build -t $REGISTRY_HOST/kube-demo-plugin .'
-            }
-        }
-        docker.withRegistry(env.REGISTRY_HOST, 'docker_registry_credentials_id') {
-            stage('Push Docker Image') {
-                steps {
-                    echo 'Pushing docker image....'
-                    sh '/usr/local/bin/docker push $REGISTRY_HOST/kube-demo-plugin'
-                }
-            }
-        }
-        stage('Deploy to Kubernetes') {
-            steps {
-                echo 'Deploying....'
-            }
-        }
-        stage('Service to Kubernetes') {
-            steps {
-                echo 'Servicing....'
-            }
-        }
+    stage 'Test'
+        echo 'Testing...'
+        sh '/usr/local/bin/mvn clean test'
+    stage 'Build Jar'
+        echo 'Building Jar file...'
+        sh '/usr/local/bin/mvn clean package'
+        archiveArtifacts artifacts: '**/target/*.jar', fingerprint: true
+        junit 'target/surefire-reports/*.xml'
+    stage 'Build Docker Image'
+        echo 'Building docker image....'
+        sh '/usr/local/bin/docker build -t kube-demo-plugin .'
+        echo 'Taging docker image....'
+        sh '/usr/local/bin/docker build -t $REGISTRY_HOST/kube-demo-plugin .'
+    docker.withRegistry(env.REGISTRY_HOST, 'docker_registry_credentials_id') {
+        stage 'Push Docker Image'
+        echo 'Pushing docker image....'
+        sh '/usr/local/bin/docker push $REGISTRY_HOST/kube-demo-plugin'
     }
+    stage 'Deploy to Kubernetes'
+        echo 'Deploying....'
+    stage 'Service to Kubernetes'
+        echo 'Servicing....'
 }
