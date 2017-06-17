2d8192b6460d1a764fbbd174ceca84c5d6c319c2
Alexandros Alexandrakis <a.alexandrakis@datamation.gr>
2017-06-17 03:27:45 +0300

    protocol added

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -12,7 +12,7 @@ node {
         sh '/usr/local/bin/docker build -t kube-demo-plugin .'
         echo 'Taging docker image....'
         sh '/usr/local/bin/docker build -t $REGISTRY_HOST/kube-demo-plugin .'
-    docker.withRegistry(env.REGISTRY_HOST, 'docker_registry_credentials_id') {
+    docker.withRegistry("${env.REGISTRY_PROTOCOL}://${env.REGISTRY_HOST}", 'docker_registry_credentials_id') {
         stage 'Push Docker Image'
         echo 'Pushing docker image....'
         sh '/usr/local/bin/docker push $REGISTRY_HOST/kube-demo-plugin'
